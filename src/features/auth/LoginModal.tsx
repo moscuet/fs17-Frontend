d2e58dea2f6edc3f42ceac53a8f4d2ee3ab59396
moscuet <mos.cuet@gmail.com>
2024-05-27 16:12:34 +0300

    set test user

--- src/features/auth/LoginModal.tsx
+++ src/features/auth/LoginModal.tsx
@@ -1,57 +1,70 @@
-import React, { useState } from 'react';
-import { Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button } from '@mui/material';
-import { useAppDispatch } from '../../app/hooks';
-import { login } from './authSlice';
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  TextField,
+  DialogActions,
+  Button,
+} from "@mui/material";
+import { useAppDispatch } from "../../app/hooks";
+import { login } from "./authSlice";
 
 interface LoginModalProps {
-    open: boolean;
-    handleClose: () => void;
-  }
+  open: boolean;
+  handleClose: () => void;
+}
 
 const LoginModal: React.FC<LoginModalProps> = ({ open, handleClose }) => {
-    const dispatch = useAppDispatch();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const dispatch = useAppDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleLogin = () => {
-        dispatch(login({ email, password }));
-        handleClose(); 
-    };
+  const handleLogin = () => {
+    dispatch(login({ email, password }));
+    handleClose();
+  };
 
-    return (
-        <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>Login</DialogTitle>
-            <DialogContent>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="email"
-                    label="Email Address"
-                    type="email"
-                    fullWidth
-                    variant="outlined"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <TextField
-                    margin="dense"
-                    id="password"
-                    label="Password"
-                    type="password"
-                    fullWidth
-                    variant="outlined"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </DialogContent>
-            <DialogActions>
-            <DialogActions>
-                <Button onClick={handleClose} style={{ color: '#333333' }}>Cancel</Button>
-                <Button onClick={handleLogin} style={{ color: '#333333' }}>Login</Button>
-            </DialogActions>
-            </DialogActions>
-        </Dialog>
-    );
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>
+        Login user: test.userone@gmail.com pass:testuserone{" "}
+      </DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="email"
+          label="Email Address"
+          type="email"
+          fullWidth
+          variant="outlined"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          margin="dense"
+          id="password"
+          label="Password"
+          type="password"
+          fullWidth
+          variant="outlined"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </DialogContent>
+      <DialogActions>
+        <DialogActions>
+          <Button onClick={handleClose} style={{ color: "#333333" }}>
+            Cancel
+          </Button>
+          <Button onClick={handleLogin} style={{ color: "#333333" }}>
+            Login
+          </Button>
+        </DialogActions>
+      </DialogActions>
+    </Dialog>
+  );
 };
 
 export default LoginModal;
